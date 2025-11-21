3397cc031bbe6f8bb6e411e1fa0e958b59fe228b
ek-ole <ek_34@mail.ru>
2025-11-21 19:30:08 +0300

    feat: create user graphql endpoint

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,42 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import {
+  graphql,
+  GraphQLFloat,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
+
+  const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+      id: { type: GraphQLString },
+      name: { type: GraphQLString },
+      balance: { type: GraphQLFloat },
+    }),
+  });
+
+  const RootQueryType = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: () => ({
+      users: {
+        type: new GraphQLList(UserType),
+        async resolve() {
+          const users = await prisma.user.findMany();
+          return users;
+        },
+      },
+    }),
+  });
+
+  const schema = new GraphQLSchema({
+    query: RootQueryType,
+  });
 
   fastify.route({
     url: '/',
@@ -15,7 +48,15 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      // return graphql();
+      console.log('GraphQL query:', req.body.query);
+      const result = await graphql({
+        schema,
+        source: req.body.query,
+        variableValues: req.body.variables,
+      });
+
+      console.log('GraphQL result:', result);
+      return result;
     },
   });
 };
